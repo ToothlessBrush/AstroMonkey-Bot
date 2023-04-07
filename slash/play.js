cd206bb2d68f68fdf80271896ba53323e6eaee9d
ToothlessBrush <johnklein514@gmail.com>
2023-04-08 00:49:35 +0900

    fixed play
    fixed play command for discord-player 6.0

--- slash/play.js
+++ slash/play.js
@@ -11,7 +11,18 @@ module.exports = {
 	run: async ({ client, interaction }) => {
 		if (!interaction.member.voice.channel) return interaction.editReply({embeds: [new EmbedBuilder().setColor(0xA020F0).setDescription(`**You Must be in a VC!**`)]})
 
-		const queue = await client.player.createQueue(interaction.guild)
+		const queue = await client.player.nodes.create(interaction.guild, {
+            metadata: {
+            channel: interaction.channel,
+            client: interaction.guild.members.me,
+            requestedBy: interaction.user,
+            },
+            selfDeaf: true,
+            volume: 80,
+            leaveOnEmpty: true,
+            leaveOnEnd: true,
+        })
+
 		if (!queue.connection) await queue.connect(interaction.member.voice.channel)
 
 		let embed = new EmbedBuilder() //need to change this to embed builder for v14 (done)
@@ -26,18 +37,20 @@ module.exports = {
         }
 
         //console.log("searching for song")
+        interaction.editReply({embeds: [new EmbedBuilder().setColor(0xFF0000).setDescription('searching youtube...')]})
         const result_search = await client.player.search(url, { //change to result for normal search
             requestedBy: interaction.user,
-            //searchEngine: QueryType.AUTO
+            searchEngine: QueryType.YOUTUBE_SEARCH
         })
         let song
         //error checking
         if (result_search.tracks.length === 0) { //if it wasnt found try a different way
             //return interaction.editReply("No results")
+            interaction.editReply({embeds: [new EmbedBuilder().setColor(0x00FF00).setDescription('searching everything...')]})
             //console.log("searching again...")
             const result_url = await client.player.search(url, {
                 requestedBy: interaction.user,
-                searchEngine: QueryType.YOUTUBE_VIDEO
+                searchEngine: QueryType.SPOTIFY_SEARCH
             })
             if (result_url.tracks.length === 0) { //if still not found send no results
                 return interaction.editReply("No Results")
@@ -60,15 +73,15 @@ module.exports = {
             }
             //interaction.followUp({ content: `Playing ${songTitle}` });
         } catch (error) {
-            queue.destroy()
+            queue.delete()
             console.log(error)
             return await interaction.editReply({ content: "could not join voice channel"})
         }
         
-        if (!queue.playing) await queue.play()
+        if (!queue.node.isPlaying()) await queue.node.play()
 
         //build embed based on info 
-        if (queue.tracks.length == 0) {
+        if (queue.tracks.size == 0) {
             embed
                 .setColor(0xA020F0) //purple
                 .setTitle(`**Playing**`)
@@ -78,14 +91,14 @@ module.exports = {
         } else {
             embed
                 .setColor(0xA020F0) //purple
-                .setTitle(`**Queued in Position ${queue.tracks.length}**`)
+                .setTitle(`**Queued in Position ${queue.tracks.size}**`)
                 .setDescription(`**[${song.title}](${song.url})**`)
                 .setThumbnail(song.thumbnail)
                 .setFooter({ text: `Duration: ${song.duration}`})
         }
 
-        //console.log(queue.tracks.length)     
-
+        //console.log(queue.tracks.length)   
+        
         await interaction.editReply({
             embeds: [embed],
             components: [
--- slash/queue.js
+++ slash/queue.js
@@ -13,14 +13,14 @@ module.exports = {
                 ),
 
     run: async ({ client, interaction }) => {
-        const queue = client.player.getQueue(interaction.guildId)
-        if (!queue || !queue.playing){
+        const queue = client.player.nodes.get(interaction.guildId)
+        if (!queue || !queue.node.isPlaying()){
             return await interaction.editReply({embeds: [new EmbedBuilder().setColor(0xA020F0).setDescription(`**No Music in Queue!**`)]})
         }
 
         //console.log(queue.tracks.length)
         //console.log(queue.getPlayerTimestamp().current)
-        let totalPages = Math.ceil(queue.tracks.length / 10)
+        let totalPages = Math.ceil(queue.tracks.size / 10)
         if (totalPages == 0) { //set pages to 1 when song playing but no queue
             totalPages = 1
         }
@@ -31,13 +31,14 @@ module.exports = {
             return await interaction.editReply({embeds: [new EmbedBuilder().setColor(0xA020F0).setTitle(`Invalid Page!`).setDescription(`there are only ${totalPages} pages`)]})
         }
 
+        console.log(queue.tracks)
         const queueString = queue.tracks.slice(page * 10, page * 10 + 10).map((song, i) => {
             return `**${page * 10 + i + 1}.** \`[${song.duration}]\` [${song.title}](${song.url})\n**Requested By: <@${song.requestedBy.id}>**`
         }).join("\n")
 
-        const currentSong = queue.current
+        const currentSong = queue.currentTrack
 
-        let bar = queue.createProgressBar({
+        let bar = queue.node.createProgressBar({
             queue: false,
             length: 19,
         })
