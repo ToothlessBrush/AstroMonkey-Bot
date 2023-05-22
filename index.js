22742a4cbccc12ab88ad169eae4f151f4e6debc2
ToothlessBrush <Johnklein514@gmail.com>
2023-05-23 00:41:48 +0900

    added presence

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //const Discord = require("discord.js")
-const { Client, GatewayIntentBits, Collection, InteractionCollector } = require('discord.js');
+const { Client, GatewayIntentBits, Collection, InteractionCollector, ActivityType } = require('discord.js');
 const { REST } = require("@discordjs/rest")
 const { Routes } = require("discord-api-types/v9")
 const fs = require("fs")
@@ -66,6 +66,11 @@ else {
     
     client.on("ready", () => {
         console.log(`Logged in as ${client.user.tag}`)
+        client.user.setStatus('available')
+        client.user.setActivity({
+            name: '/help',
+            type: ActivityType.Streaming
+          });
     })
 
     client.on("interactionCreate", (interaction) => {
